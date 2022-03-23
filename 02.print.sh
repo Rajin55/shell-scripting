59b0e2a102981f5740f7a2cdd81382fd70a69b31
saisindhu96 <saisindhuraju@gmail.com>
2022-03-23 10:36:20 +0530

    add new line

---
+++ 02.print.sh
@@ -0,0 +1,21 @@
+echo "Devops"
+
+
+echo Hello world
+
+echo -e "Hello,\n welcome to devops"
+echo -e "\nHello,\n\nWelcome to devops"
+
+
+#syntax echo -e "\e[COLORCODEmMessage"
+# COLORS       CODE
+#RED            31
+#Green          32
+#Yellow          33
+#Blue            34
+#Magenta        35
+# Cyan          36
+
+echo -e "\e[32mWelcome to Devops\e[0m"
+echo Devops
+echo -e "\e[34mWelcome to Devops"
