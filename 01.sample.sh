cfd49e5707c61f1cd65cb7084ee553534ee4ece3
saisindhu96 <saisindhuraju@gmail.com>
2022-03-23 10:27:51 +0530

    add new line

--- 01.sample.sh
+++ 01.sample.sh
@@ -9,3 +9,16 @@ echo Hello world
 
 echo -e "Hello,\n welcome to devops"
 echo -e "\nHello,\n\nWelcome to devops"
+
+
+#syntax echo -e "\e[COLORCODEMessage"
+# COLORS       CODE
+#RED            31
+#Green          32
+#Yellow          33
+#Blue            34
+#Magenta        35
+# Cyan          36
+
+echo -e "/e[32Welcome to Devops"
+
