8d7790ceb8aedb15f94fe6e8bd92a154e8bf879b
saisindhu96 <saisindhuraju@gmail.com>
2022-03-23 10:34:16 +0530

    add new line

--- 01.sample.sh
+++ 01.sample.sh
@@ -20,7 +20,7 @@ echo -e "\nHello,\n\nWelcome to devops"
 #Magenta        35
 # Cyan          36
 
-echo -e "\e[32mWelcome to Devops"
+echo -e "\e[32mWelcome to Devops\e[0m"
 echo Devops
 echo -e "\e[34mWelcome to Devops"
 
