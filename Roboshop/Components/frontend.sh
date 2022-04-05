f43647165644c64a249511623e3c03e2c5a5033f
saisindhu96 <saisindhuraju@gmail.com>
2022-04-05 18:36:45 +0530

    adding components

--- Roboshop/Components/frontend.sh
+++ Roboshop/Components/frontend.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo intalling Frontend
-#source common.sh
-#rm -f /tmp/roboshop.log
 
-#HEAD "Installing Nginx"
-#yum install nginx -y &>>/tmp/roboshop.log
-#STAT $?
+source common.sh
+rm -f /tmp/roboshop.log
+
+HEAD "Installing Nginx"
+yum install nginx -y &>>/tmp/roboshop.log
+STAT $?
