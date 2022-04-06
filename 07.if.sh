4cb283d2a70c7dd47fcf8682bf3fca5315e5e467
saisindhu96 <saisindhuraju@gmail.com>
2022-04-06 09:31:22 +0530

    adding components

--- 07.if.sh
+++ 07.if.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#read -p "Enter Username:" username
-#
-#if [ "$username" == "root" ];then
- #  echo "Hey,user $username is admin user"
-#else
-#  echo "Hey, user $username is Normal user"
-#fi
+read -p "Enter Username:" username
+
+if [ "$username" == "root" ];then
+   echo "Hey,user $username is admin user"
+else
+   echo "Hey, user $username is Normal user"
+fi
 
 read -p "Enter filename:" filename
 
