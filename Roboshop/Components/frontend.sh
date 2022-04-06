03a8ba82e7ef5f9ec90fcfa6ae2918ce8a837787
saisindhu96 <saisindhuraju@gmail.com>
2022-04-06 09:58:30 +0530

    adding components

--- Roboshop/Components/frontend.sh
+++ Roboshop/Components/frontend.sh
@@ -11,3 +11,22 @@ STAT $?
 HEAD "Download  from Github"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 STAT $?
+
+HEAD "Delete old HTML Docs\t"
+rm -rf /usr/share/nginx/html/*
+STAT $?
+
+HEAD "Extract Downoad Content"
+unzip -d /usr/share/nginx/html /tmp/frontend.zip &>>/tmp/roboshop.log
+mv /usr/share/nginx/html/frontend-main/* /usr/share/nginx/html/. &>>/tmp/roboshop.log
+mv /usr/share/nginx/html/static/* /usr/share/nginx/html/. &>>/tmp/roboshop.log
+STAT $?
+
+HEAD" Update Nginx Configuration"
+mv /usr/share/nginx/html/localhost.conf /etc/nginx/default.d/roboshop.conf
+STAT $?
+
+HEAD"Start Nginx"
+systemctl restart nginx &>>/tmp/roboshop.log
+systemctl enable nginx &>>/tmp/roboshop.log
+
