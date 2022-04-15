bb732d568379a3188c08450f5a1be67a84581f5c
saisindhu96 <saisindhuraju@gmail.com>
2022-04-15 18:42:41 +0530

    adding components

--- Roboshop/lunch instance/lunch.sh
+++ Roboshop/lunch instance/lunch.sh
@@ -9,8 +9,9 @@ fi
 
 LID=lt-06a6fcc7498721348
 LVER=1
+
 INSTANCE_STATE=$(aws ec2 describe-instance --filters "Name=tag:Name,values=${COMPONENT}" | jq .Reservations[].Instances[].state.Name | xargs -n1 )
-if[ "${INSTANCE_STATE}" = "running" ]; then
+if [ "${INSTANCE_STATE}" = "running" ]; then
   echo"Instance is already Existed"
   exit 0
 fi
