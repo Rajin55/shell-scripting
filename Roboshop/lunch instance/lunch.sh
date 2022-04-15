d4a5a3bcf4d8c78022d3f7aebbcc7dba743430fe
saisindhu96 <saisindhuraju@gmail.com>
2022-04-15 18:45:37 +0530

    adding components

--- Roboshop/lunch instance/lunch.sh
+++ Roboshop/lunch instance/lunch.sh
@@ -21,4 +21,4 @@ if [ "${INSTANCE_STATE}" = "stop" ]; then
   exit 0
 fi
 
-aws ec2 run-instance --lunch-template LunchTemplateId=${LID},Version=${LVER}  --tag-sepecifications "ResourceType=instance, Tags=[{Key=Name,Value=${COMPONENT}]" | jq
+aws ec2 run-instances --lunch-template LunchTemplateId=${LID},Version=${LVER}  --tag-sepecifications "ResourceType=instance, Tags=[{Key=Name,Value=${COMPONENT}]" | jq
