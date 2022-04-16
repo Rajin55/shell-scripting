4bf6aad0659e3b4a79019c1605a28aaaffdac2d4
saisindhu96 <saisindhuraju@gmail.com>
2022-04-16 11:21:48 +0530

    adding components

--- Roboshop/Components/catalogue.sh
+++ Roboshop/Components/catalogue.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-echo Installing catalogue
+source Components/common.sh
+rm -f /tmp/roboshop.log
+#set-hostname catalogue
+
+HEAD " Install Nodejs"
+yum install nodejs make gcc-c++ -y &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Add Roboshop user"
+id roboshop &>>/tmp/roboshop.log
+if [ $? -eq 0 ];then
+  echo User is alraedy there,so skkip yhe user &>>/tmp/roboshop.log
+  STAT $?
+else
+   useradd roboshop &>>/tmp/roboshop.log
+   STAT $?
+fi
