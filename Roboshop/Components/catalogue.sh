ee30a60d8ccef6cdf44dabd84ea4bdd5d2579f16
saisindhu96 <saisindhuraju@gmail.com>
2022-04-16 11:36:28 +0530

    adding components

--- Roboshop/Components/catalogue.sh
+++ Roboshop/Components/catalogue.sh
@@ -4,16 +4,31 @@ source Components/common.sh
 rm -f /tmp/roboshop.log
 #set-hostname catalogue
 
-HEAD " Install Nodejs"
+HEAD "Install Nodejs"
 yum install nodejs make gcc-c++ -y &>>/tmp/roboshop.log
 STAT $?
 
 HEAD "Add Roboshop user"
 id roboshop &>>/tmp/roboshop.log
 if [ $? -eq 0 ];then
-  echo User is alraedy there,so skkip yhe user &>>/tmp/roboshop.log
+  echo User is alraedy there,so skkip the user &>>/tmp/roboshop.log
   STAT $?
 else
    useradd roboshop &>>/tmp/roboshop.log
    STAT $?
 fi
+
+HEAD "Download App from GitHub"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Extract the Downloaded file"
+cd /home/roboshop && rm -rf catalogue && unzip /tmp/catalogue.zip &>>/tmp/roboshop.log && mv catalogue-main catalogue
+STAT $?
+
+HEAD "install Nodejs Depedncies"
+cd /home/roboshop/catalogue && npm install &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Fix permisions to App conent"
+chown roboshop:roboshop /home/roboshop -R
