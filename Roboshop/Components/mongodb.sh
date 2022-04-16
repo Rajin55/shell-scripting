2d2abb1843f9e281dc6d38877a3471c705a62ebb
saisindhu96 <saisindhuraju@gmail.com>
2022-04-16 10:11:48 +0530

    adding components

--- Roboshop/Components/mongodb.sh
+++ Roboshop/Components/mongodb.sh
@@ -2,6 +2,7 @@
 
 source Components/common.sh
 rm -f /tmp/roboshop.log
+set-hostname mongodb
 
 HEAD "Setup MongoDB yum repo"
 echo '[mongodb-org-4.2]
@@ -12,10 +13,11 @@ enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 STAT $?
 
-HEAD "Install MongoDB"
+HEAD "Install MongoDB\t\t"
 yum install -y mongodb-org &>>/tmp/roboshop.log
 STAT $?
 
 HEAD "start MongoDb Service "
 systemctl enable mongod &>>/tmp/roboshop.log
 systemctl start mongod  &>>/tmp/roboshop.log
+STAT $?
