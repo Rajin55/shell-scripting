e577d0a1c98461e8ed28b77872246e1a223055c2
saisindhu96 <saisindhuraju@gmail.com>
2022-04-21 09:19:57 +0530

    adding components

--- Roboshop/lunch instance/lunch.sh
+++ Roboshop/lunch instance/lunch.sh
@@ -26,6 +26,6 @@ INSTANCE_CREATE() {
   fi
   echo -n Instance ${COMPONENT} created - IPADDRESS is
   aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq | grep  PrivateIpAddress  |xargs -n1
-  sleep 10
+  sleep 30
   DNS_UPDATE
 }
