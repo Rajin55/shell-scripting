90c56cb199ffd352ab79a5849b3f4ed72b83b16d
saisindhu96 <saisindhuraju@gmail.com>
2022-04-22 10:12:29 +0530

    adding components

--- Roboshop/Components/common.sh
+++ Roboshop/Components/common.sh
@@ -46,7 +46,7 @@ NODEJS(){
   STAT $?
 
   HEAD "Setup SystemD Service"
-  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_MONGO_ENDPOINTENDPOINT/redis.roboshop.internal/' -e 's//mongodb.roboshop.internal/' /home/roboshop/$1/systemd.service && mv /home/roboshop/$1/systemd.service /etc/systemd/system/$1.service
+  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/$1/systemd.service && mv /home/roboshop/$1/systemd.service /etc/systemd/system/$1.service
   STAT $?
 
   HEAD "Start Catalogue Service"
