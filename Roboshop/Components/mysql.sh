b187d3944feba51889d0047f9fa4ed1d50fb5e72
saisindhu96 <saisindhuraju@gmail.com>
2022-04-22 10:52:49 +0530

    adding components

--- Roboshop/Components/mysql.sh
+++ Roboshop/Components/mysql.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-echo Installing mysql
+source Components/common.sh
+rm -f /tmp/roboshop.log
+#set-hostname mysql
+
+HEAD "Setup MYSQL Repo"
+echo '[mysql57-community]
+name=MySQL 5.7 Community Server
+baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/
+enabled=1
+gpgcheck=0' > /etc/yum.repos.d/mysql.repo
+STAT $?
+
+HEAD "Install Mysql Service"
+yum remove mariadb-libs -y &>>/tmp/roboshop.log && yum install mysql-community-server -y &>>/temp/roboshop.log
+STAT $?
+
+HEAD "Start MYSQL Service"
+systemctl enable mysqld &>>/tmp/roboshop.log && systemctl start mysqld &>>/tmp/roboshop.log
