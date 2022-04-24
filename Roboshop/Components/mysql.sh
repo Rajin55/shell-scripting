7e60c85203e64da9a04ba7523ea8900f3423915f
saisindhu96 <saisindhuraju@gmail.com>
2022-04-24 17:54:42 +0530

    adding components

--- Roboshop/Components/mysql.sh
+++ Roboshop/Components/mysql.sh
@@ -24,25 +24,10 @@ DEF_PASS=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';
 uninstall plugin validate_password;" >/tmp/db.sql
 
-echo show databases | mysql -uroot -pRoboShop@1 &>>/tmp/roboshop.log
-if [ $? -ne 0 ]; then
   HEAD "Reset MySQL Password"
   mysql --connect-expired-password -uroot -p"${DEF_PASS}" </tmp/db.sql &>>/tmp/roboshop.log
   STAT $?
-fi
 
-HEAD "Download Schema from GitHub\t"
-curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>/tmp/roboshop.log
-STAT $?
-
-HEAD "Extract Downloaded Archive\t"
-cd /tmp
-unzip -o mysql.zip &>>/tmp/roboshop.log
-STAT $?
-
-HEAD "Load Shipping Schema"
-cd /tmp && unzip -o mysql.zip &>>/tmp/roboshop.log && cd mysql-main && mysql -u root -pRoboShop@1 <shipping.sql  &>>/tmp/roboshop.log
-STAT $?
 
 
 
