6c7dfcdd8daac31b1d512f7f6caa0a3f55e324e5
saisindhu96 <saisindhuraju@gmail.com>
2022-04-24 17:01:12 +0530

    adding components

--- Roboshop/Components/mysql.sh
+++ Roboshop/Components/mysql.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-source Components/common.sh
+source components/common.sh
 rm -f /tmp/roboshop.log
 #set-hostname mysql
 
-HEAD "Setup MYSQL Repo"
+HEAD "Setup MySQL Repo"
 echo '[mysql57-community]
 name=MySQL 5.7 Community Server
 baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/
@@ -12,17 +12,17 @@ enabled=1
 gpgcheck=0' > /etc/yum.repos.d/mysql.repo
 STAT $?
 
-HEAD "Install Mysql Service"
+HEAD "Install MySQL Server"
 yum remove mariadb-libs -y &>>/tmp/roboshop.log && yum install mysql-community-server -y &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Start MYSQL Service"
+HEAD "Start MySQL Service"
 systemctl enable mysqld &>>/tmp/roboshop.log && systemctl start mysqld &>>/tmp/roboshop.log
 STAT $?
 
-DEF_PASS=$(grep 'A temporary password'temp /var/log/mysqld.log | awk '{print $NF}')
-echo "ALTER USER 'root'@'localhost' IDENTIFIED NY 'RoboShop@1';
-unstill plugin validate_password;" >/tmp/db.sql
+DEF_PASS=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';
+uninstall plugin validate_password;" >/tmp/db.sql
 
 echo show databases | mysql -uroot -pRoboShop@1 &>>/tmp/roboshop.log
 if [ $? -ne 0 ]; then
@@ -43,3 +43,10 @@ STAT $?
 HEAD "Load Shipping Schema"
 cd /tmp && unzip -o mysql.zip &>>/tmp/roboshop.log && cd mysql-main && mysql -u root -pRoboShop@1 <shipping.sql  &>>/tmp/roboshop.log
 STAT $?
+
+
+
+
+
+
+
